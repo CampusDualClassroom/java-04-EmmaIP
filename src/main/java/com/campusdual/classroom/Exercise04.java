f47403a63ebceae7cc4bb2e0e13c6140f2287e08
EmmaIP <emmapineiro@gmail.com>
2024-10-14 13:50:50 +0200

    ejercicio 4 finalizado

--- src/main/java/com/campusdual/classroom/Exercise04.java
+++ src/main/java/com/campusdual/classroom/Exercise04.java
@@ -5,26 +5,48 @@ public class Exercise04 {
 
         System.out.println("Ejemplos de cálculos SIN estructuras de control:");
         System.out.println("Suma de los primeros 5 números: " + (1 + 2 + 3 + 4 + 5));
+        System.out.println("Suma de los primeros 5 números: " + sumNum(5));
         System.out.println("Suma de los primeros 20 números pares: " + (2 + 4 + 6 + 8 + 10 + 12 + 14 + 16 + 18 + 20 + 22 + 24 + 26 + 28 + 30 + 32 + 34 + 36 + 38 + 40));
+        System.out.println("Suma de los primeros 20 números pares: " + sumEvenNum(20));
         System.out.println("Obtén el factorial del número 7 → (7!): " + (7 * 6 * 5 * 4 * 3 * 2 * 1));
+        System.out.println("Obtén el factorial del número 7: " + factorial(7));
+        System.out.println("Obtén el factorial del número 7!: " + recursiveFactorial(7));
         System.out.println("######################################################################");
         System.out.println("Ejemplos de cálculos CON estructuras de control:");
 
     }
 
     public static int sumNum(int num) {
-        return 0;
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum += i;
+        }
+        return sum;
     }
 
     public static int sumEvenNum(int num) {
-        return 0;
+        int sum = 0;
+        int evenNum = num + 20;
+        for (int i = 1; i <= evenNum; i++) {
+            if(i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
 
     public static int factorial(int num) {
-        return 0;
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        return fact;
     }
 
     public static int recursiveFactorial(int num) {
-        return 0;
+        if(num == 0) {
+            return 1;
+        }
+        return num * recursiveFactorial(num -1);
     }
 }
